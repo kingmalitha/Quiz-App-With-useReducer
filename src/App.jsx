1abbf271cff662c1ba23471db443cf2726848887
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2023-06-24 08:47:15 +0530

    10. Moving to the Next Question

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Loader from "./components/Loader";
 import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
+import NextButton from "./components/NextButton";
 
 const initialState = {
   questions: [],
@@ -44,6 +45,12 @@ function reducer(state, action) {
             : state.points,
       };
     }
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
     default:
       throw new Error(`Unhandled action type: ${action.type}`);
   }
@@ -74,11 +81,15 @@ function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </MainContainer>
     </>
