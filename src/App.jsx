6700fa8683ea890bda3c303bf46ebb05013655c3
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2023-06-24 06:36:18 +0530

    05. Loading Questions from a Fake API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+  // loading, error, ready, active, finished
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/questions")
+      .then((res) => res.json)
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <Header />
